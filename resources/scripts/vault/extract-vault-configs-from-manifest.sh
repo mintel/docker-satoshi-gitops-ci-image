2dcf1d7d14ae745c05b50ffeff86cf963690ea70
Francesco Ciocchetti <fciocchetti@mintel.com>
2019-04-01 14:37:20 +0200

    Add more info on what was skipped

--- resources/scripts/vault/extract-vault-configs-from-manifest.sh
+++ resources/scripts/vault/extract-vault-configs-from-manifest.sh
@@ -15,18 +15,19 @@ do
   for DOC in `seq 0 $N_DOCS`
   do
     kind=$(yq read -d $DOC $file kind)
-    [[ $kind == "SealedSecret" ]] && continue
 
     skip_ci=$(yq read -d $DOC $file 'metadata.annotations."mintel.com/skip-ci"')
-    [[ $skip_ci == "true" ]] && continue
 
 
     name=$(yq read -d $DOC $file metadata.name)
     namespace=$(yq read -d $DOC $file metadata.namespace)
     data=$(yq read -d $DOC $file 'data."vault-config.yml"')
-    [[ $data == "null" ]] && continue
 
     file_name="${namespace}_${name}.yaml"
+
+    [[ $kind == "SealedSecret" ]] && echo "EXCLUDING: $namespace-$name - SealedSecret" continue
+    [[ $skip_ci == "true" ]] && echo "EXLCUDING: $namespace-$name - skip-ci annotation" continue
+    [[ $data == "null" ]] && echo "EXLCUDING: $namespace-$name - not a vault-config.yml key" continue
 
     if [[ $kind == "ConfigMap" ]]; then
       yq read -d $DOC $file 'data."vault-config.yml"' > ${CONFS_DIR}/${file_name}
