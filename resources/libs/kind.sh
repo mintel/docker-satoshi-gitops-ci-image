529765402041314930c71b63bed917431aaee52c
Charlie Ward <cward@mintel.com>
2020-01-17 14:45:46 +0000

    Make CNI replacement optional (changed to Weave Net) and change kubeconfig for new Kind version

--- resources/libs/kind.sh
+++ resources/libs/kind.sh
@@ -3,17 +3,17 @@
 K8S_VERSION="${K8S_VERSION:-v1.13.10@sha256:2f5f882a6d0527a2284d29042f3a6a07402e1699d792d0d5a9b9a48ef155fa2a}"
 K8S_WORKERS="${KIND_NODES:-1}"
 KIND_FIX_KUBECONFIG="${KIND_FIX_KUBECONFIG:-false}"
+KIND_REPLACE_CNI="${KIND_REPLACE_CNI:-false}"
 DOCKER_HOST_ALIAS="${DOCKER_HOST_ALIAS:-docker}"
-CNI_FLANNEL_VERSION="${CNI_FLANNEL_VERSION:-v0.11.0}"
 
 function install_cni() {
-	kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/${CNI_FLANNEL_VERSION}/Documentation/kube-flannel.yml
+	kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 }
 
 function start_kind() {
 	cat > /tmp/kind-config.yaml <<EOF
 kind: Cluster
-apiVersion: kind.sigs.k8s.io/v1alpha3
+apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
   image: kindest/node:${K8S_VERSION}
@@ -32,18 +32,26 @@ EOF
 	cat >> /tmp/kind-config.yaml <<EOF
 networking:
   apiServerAddress: 0.0.0.0
-  # Disable default CNI and install flannel to get around DIND issues
+EOF
+
+  if [[ "$KIND_REPLACE_CNI" == "true" ]]; then
+	  cat >> /tmp/kind-config.yaml <<EOF
+  # Disable default CNI and install Weave Net to get around DIND issues
   disableDefaultCNI: true
 EOF
+  fi
+
+	export KUBECONFIG="${HOME}/.kube/kind-config"
 
 	kind create cluster --config /tmp/kind-config.yaml
 
-	export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
 	if [[ "$KIND_FIX_KUBECONFIG" == "true" ]]; then	
 		sed -i -e "s/server: https:\/\/0\.0\.0\.0/server: https:\/\/$DOCKER_HOST_ALIAS/" "$KUBECONFIG"
 	fi
 
-	install_cni
+  if [[ "$KIND_REPLACE_CNI" == "true" ]]; then
+	  install_cni
+  fi
 
 	kubectl cluster-info
 
